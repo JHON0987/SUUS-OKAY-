7a74fe5828b0d4e642650eb4ae270ee3b33d5aca
David <davidvillaosorio@iesantaelena.edu.co>
2022-11-01 15:21:22 -0500

    firebase
    -se conecto con base de datos fire base

---
+++ Suus okay/firebase.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDIYXLGKGjiR-ZJjTjBHDzQ9ZxATJRr4sI",
+    authDomain: "suus-okay-crud.firebaseapp.com",
+    projectId: "suus-okay-crud",
+    storageBucket: "suus-okay-crud.appspot.com",
+    messagingSenderId: "522941940036",
+    appId: "1:522941940036:web:3f0cee29033c482b671711"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
